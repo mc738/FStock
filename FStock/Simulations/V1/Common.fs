57a647ad3d6fc1a1ad9326d403935d1d2378bbef
Max Clifford <me@mclifford.dev>
2024-01-17 19:26:35 +0000

    Work on Common.fs

--- FStock/Simulations/V1/Common.fs
+++ FStock/Simulations/V1/Common.fs
@@ -1,2 +1,56 @@
-﻿module FStock.Simulations.V1.Common
+﻿namespace FStock.Simulations.V1
 
+open System
+
+[<AutoOpen>]
+module Common =
+
+    type PositionStart =
+        { Symbol: string
+          Start: DateTime
+          BuyPrice: decimal
+          Volume: decimal }
+
+    type CurrentPosition =
+        { Symbol: string
+          Date: DateTime
+          Open: decimal
+          High: decimal
+          Low: decimal
+          Close: decimal
+          AdjustedClose: decimal }
+
+    type Con
+
+    type PositionCondition =
+        | PercentageGrowth of Percent: decimal
+        | FixedValue of Value: decimal
+        | PercentageLoss of Percent: decimal
+        | FixedLoss of Value: decimal
+        | Duration of Length: int
+        | Bespoke of Handler: (PositionStart -> CurrentPosition -> bool)
+        | And of PositionCondition * PositionCondition
+        | Or of PositionCondition * PositionCondition
+        | All of PositionCondition list
+        | Any of PositionCondition list
+        | Not of PositionCondition
+
+        member pc.Test(start: PositionStart, current: CurrentPosition, settings) =
+            let rec handle (condition: PositionCondition) =
+                match condition with
+                | PercentageGrowth percent ->
+                    
+                    
+                    failwith "todo"
+                | FixedValue value -> failwith "todo"
+                | PercentageLoss percent -> failwith "todo"
+                | FixedLoss value -> failwith "todo"
+                | Duration length -> failwith "todo"
+                | Bespoke handler -> failwith "todo"
+                | And(positionCondition, condition) -> failwith "todo"
+                | Or(positionCondition, condition) -> failwith "todo"
+                | All positionConditions -> failwith "todo"
+                | Any positionConditions -> failwith "todo"
+                | Not positionCondition -> failwith "todo"
+                
+            handle pc
