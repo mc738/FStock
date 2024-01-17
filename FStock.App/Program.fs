b0aca8f18274045a9f1d9f109473a07d03adf335
Max Clifford <me@mclifford.dev>
2024-01-17 23:30:05 +0000

    Added example simulation

--- FStock.App/Program.fs
+++ FStock.App/Program.fs
@@ -1,212 +1,273 @@
 ﻿open System
+open System.IO
+open System.Text.Json
+open FSVG
+open FSVG.Charts
 open FSVG.Dsl
+open FStock.Analysis
+open FStock.Analysis.V1
 open FStock.Data
 open FStock.Data.Persistence
+open FStock.Data.Store
+open FStock.Simulations.V1
 open FStock.Visualizations.Charts
 open Freql.Sqlite
 
+module DistributionChartTest =
 
-let exampleQuery =
-    "SELECT * FROM stocks s WHERE s.symbol = 'TSLA' AND Date(entry_date) >= DATE('2020-01-01') AND DATE(entry_date) < DATE('2020-02-01')"
-
-let total = 24150254
-
-let chunks =
-    List.init 2500 (fun i -> i * 10000)
-    |> List.map (fun v -> v, 10000)
-
-let ctx =
-    SqliteContext.Open "E:\\data\\stock_market\\fstock_store.db"
-
-type Bucket =
-    { Min: decimal
-      Max: decimal
-      Count: int }
-
-let group (points: decimal list) =
-    let max = (points |> List.max) + 1m
-    let min = points |> List.min
-
-    let diff = max - min
-
-    let size = diff / 10m
-
-    let buckets =
-        List.init 10 (fun i -> ((decimal i * size) + min, (decimal (i + 1) * size) + min))
-        |> List.map (fun (mi, ma) ->
-            { Min = mi
-              Max = ma
-              Count =
-                points
-                |> List.filter (fun v -> v >= mi && v < ma)
-                |> List.length })
+    let run (ctx: SqliteContext) (symbol: string) =
 
 
-    let total = buckets |> List.sumBy (fun b -> b.Count)
-        
-    
-    let max = buckets |> List.maxBy (fun b -> b.Count) |> fun b -> b.Count
-    
-    
-    
+        let t = Store.previousXDays ctx 200 (DateTime(2019, 2, 5)) symbol
+
+        t
+        |> List.map (fun s -> s.CloseValue)
+        |> DistributionChart.generate
+        |> saveToFile "C:\\ProjectData\\fstock\\test_distribution_chart.svg" 100 100
+
+module CandleStickChartTest =
+
+    let run (ctx: SqliteContext) (symbol: string) =
+
+        Persistence.Operations.selectStockRecords
+            ctx
+            [ "WHERE symbol = @0 AND Date(entry_date) >= DATE(@1) AND DATE(entry_date) < DATE(@2)" ]
+            [ symbol; DateTime(2020, 1, 1); DateTime(2020, 2, 1) ]
+        |> List.map (fun s ->
+            ({ Date = s.EntryDate
+               Open = s.OpenValue
+               Close = s.CloseValue
+               High = s.HighValue
+               Low = s.LowValue
+               AdjClose = s.AdjustedCloseValue
+               Volume = s.VolumeValue
+               MovingAverage200 = 0m
+               MovingAverage50 = 0m }
+            : DataPoint))
+        |> generateCandleStickChart ({ ValuePaddingPercent = 20m })
+        |> saveToFile "C:\\ProjectData\\fstock\\test_chart.svg" 120 100
+
+module TestStrategy =
+
+    let run (ctx: SqliteContext) (startDate: DateTime) (item: DayReportItem) =
+        printfn $"Test {item.Symbol}"
+
+        [ 5.; 10.; 20.; 30.; 50.; 100.; 200.; 365. ]
+        |> List.iter (fun v ->
+            match Store.getStockForDate ctx (startDate.AddDays(v)) item.Symbol with
+            | Some s -> printfn $"\tValue after {v} days: {s.CloseValue} ({s.CloseValue - item.Stock.CloseValue})"
+            | None -> printfn $"\tNo data for +{v} days.")
+
+
+module NewTest =
+
+    let run (ctx: SqliteContext) =
+
+        let test = History.build ctx "GOOG" (DateTime(2019, 01, 02)) 100
+
+        let jsOpt = JsonSerializerOptions()
+
+        jsOpt.WriteIndented <- true
+
+        JsonSerializer.Serialize(test, jsOpt)
+        |> fun b -> File.WriteAllText("E:\\data\\stock_market\\exports\\example.json", b)
+
+
+        let settings =
+            ({ ChartDimensions =
+                { Height = 100.
+                  Width = 100.
+                  LeftOffset = 10
+                  BottomOffset = 10
+                  TopOffset = 10
+                  RightOffset = 10 }
+               Title = Some "Google Comparison"
+               XLabel = Some "Value"
+               YLabel = Some "Date"
+               LegendStyle =
+                 ({ Bordered = false
+                    Position = LegendPosition.Right }
+                 : LegendStyle)
+                 |> Some
+               YMajorMarkers = [ 50; 100 ]
+               YMinorMarkers = [ 25; 75 ] }
+            : LineCharts.Settings)
+
+        (*
+        "Item 1"
+        "Item 2"
+        "Item 3"
+        "Item 4"
+        "Item 5"
+        *)
+        let items = test.Items |> List.ofSeq |> List.rev
+
+        let seriesCollection =
+            ({ SplitValueHandler = valueSplitter float
+               Normalizer = rangeNormalizer<decimal> float
+               PointNames = items |> List.map (fun i -> i.EntryDate.ToString("dd'-'MM")) //[ "Item 1"; "Item 2"; "Item 3"; "Item 4"; "Item 5" ]
+               Series =
+                 [ ({ Name = "7 day moving average"
+                      Style =
+                        { Color = SvgColor.Grey
+                          StrokeWidth = 0.3
+                          LineType = LineCharts.LineType.Straight
+                          Shading =
+                            ({ Color = SvgColor.Rgba(255uy, 0uy, 0uy, 0.3) }: LineCharts.ShadingOptions)
+                            |> Some }
+                      Values = items |> List.map (fun i -> i.Previous7MovingAverage.Close) }
+                   : LineCharts.Series<decimal>)
+                   ({ Name = "20 day moving average"
+                      Style =
+                        { Color = SvgColor.Black
+                          StrokeWidth = 0.3
+                          LineType = LineCharts.LineType.Bezier
+                          Shading = None }
+                      Values = items |> List.map (fun i -> i.Previous20MovingAverage.Close) }
+                   : LineCharts.Series<decimal>)
+                   ({ Name = "Close value"
+                      Style =
+                        { Color = SvgColor.Named "green"
+                          StrokeWidth = 0.3
+                          LineType = LineCharts.LineType.Bezier
+                          Shading = None }
+                      Values = items |> List.map (fun i -> i.Close) }
+                   : LineCharts.Series<decimal>) ] }
+            : LineCharts.SeriesCollection<decimal>)
+
+        let min = (items |> List.minBy (fun i -> i.Low)) |> fun r -> r.Low
+        let max = (items |> List.maxBy (fun i -> i.High)) |> fun r -> r.High
+
+        LineCharts.generate settings seriesCollection min max
+        |> fun r -> File.WriteAllText("E:\\data\\stock_market\\exports\\example_chart.svg", r)
+
+
+
+        ()
+
+
+let ctx = SqliteContext.Open "E:\\data\\stock_market\\fstock_store.db"
+
+let old _ =
+
+    let dayReports = Store.getDay ctx (DateTime(2019, 2, 5))
+
+    let interesting =
+        dayReports.Items
+
+        |> Seq.filter (fun dri ->
+            dri.MovingAverage50Days < dri.MovingAverage200Days
+            && dri.Stock.OpenValue > dri.PreviousDay.CloseValue
+            && dri.Previous50DayStandardDeviation > (float (dri.MovingAverage50Days / 100m) * 10.)
+            && dri.Previous200DayStandardDeviation > (float (dri.MovingAverage200Days / 100m) * 10.)
+            && dri.Previous50DayLow > dri.Previous200DayLow
+            && dri.Stock.OpenValue < ((dri.MovingAverage200Days / 100m) * 90m)
+        //&& dri.Previous50DayHigh > dri.Previous200DayHigh
+        )
+        |> List.ofSeq
+
+    interesting |> List.iter (TestStrategy.run ctx dayReports.Date)
+
+module Analytics =
+
+    open FStock.Analysis.V1
+
+    let build _ =
+
+        let storeCtx = SqliteContext.Open "E:\\data\\stock_market\\fstock_store.db"
+
+        ({ SampleSize = 1.
+           Date = (DateTime(2019, 01, 02))
+           LookBackCount = 30
+           Path = "C:\\ProjectData\\fstock\\analytics_test.db" }
+        : BuildParameters)
+        |> build storeCtx
 
     ()
 
+module Simulation =
+
+    let run _ =
+
+        let storeCtx = SqliteContext.Open "E:\\data\\stock_market\\fstock_store.db"
+        let start = DateTime(2019, 01, 02)
+
+        match Store.getStockForDate ctx start "GOOG" with
+        | Some s ->
+            // For now choose open value.
+
+            let positionStart =
+                ({ Symbol = s.Symbol
+                   Start = start
+                   BuyPrice = s.OpenValue
+                   Volume = 1m }
+                : PositionStart)
+
+            let settings = ({ ValueMode = ValueMode.Close }: SimulationSettings)
+
+            let conditions =
+                PositionCondition.Any [ PositionCondition.PercentageGrowth 50m; PositionCondition.PercentageLoss 50m ]
+
+            let rec handler (date: DateTime) =
+                if (date.Date < DateTime.UtcNow.Date) then
+                    match Store.getStockForDate storeCtx date positionStart.Symbol with
+                    | Some cs ->
+                        let cp =
+                            ({ Symbol = cs.Symbol
+                               Date = cs.EntryDate
+                               Open = cs.OpenValue
+                               High = cs.HighValue
+                               Low = cs.CloseValue
+                               Close = cs.CloseValue
+                               AdjustedClose = cs.AdjustedCloseValue }
+                            : CurrentPosition)
+
+                        let v =
+                            match settings.ValueMode with
+                            | ValueMode.Open -> cp.Open
+                            | ValueMode.Close -> cp.Close
+                            | ValueMode.High -> cp.High
+                            | ValueMode.Low -> cp.Low
+                            | ValueMode.AdjustedClose -> cp.AdjustedClose
+
+                        if conditions.Test(positionStart, cp, settings) then
+                            let diff = v - positionStart.BuyPrice
+
+                            let percentDiff = ((v - positionStart.BuyPrice) / positionStart.BuyPrice) * 100m
+
+                            //
+                            printfn
+                                $"Stock `{cs.Symbol}` sold after {(cs.EntryDate - positionStart.Start).Days} day(s)."
+
+                            printfn $"Buy price: {positionStart.BuyPrice}"
+                            printfn $"Sell price: {v}"
+                            printfn $"Difference: {diff} (%0.2f{percentDiff}%%)"
+                            printfn $"Volume: {positionStart.Volume}"
+                            printfn $"Profit: {diff * positionStart.Volume}"
+                        else
+                            handler (date.AddDays(1))
+                    | None -> handler (date.AddDays(1))
+                else
+                    printfn "Not sold, still holding."
+
+            handler (start.AddDays(1))
+        | None ->
+
+            printfn "Not found."
+
+        ()
 
 
-let t =
-    Store.previousXDays ctx 200 (DateTime(2019, 2, 5)) "CRIS"
+Simulation.run ()
+let r = Analytics.build ()
+
+NewTest.run ctx
 
 
-t
-|> List.map (fun s -> s.CloseValue)
-|> DistributionChart.generate
-|> saveToFile "C:\\ProjectData\\fstock\\test_distribution_chart.svg" 100 100
-
-//|> group
-
-//let t = [ 1m;2m;3m;3m;9m;10m ]
-
-let avg =
-    t |> List.averageBy (fun s -> s.CloseValue)
-
-let var =
-    t
-    |> List.map (fun s -> Math.Pow(float (s.CloseValue - avg), 2))
-    |> List.sumBy (fun s -> s)
-    |> fun r -> r / (float t.Length)
-
-let sd = Math.Sqrt(var)
-
-//let avg = test |> List.averageBy (fun s -> s.CloseValue)
-
-//let variance =
-
-
-let dayReports =
-    Store.getDay ctx (DateTime(2019, 2, 5))
-
-let interesting =
-    dayReports.Items
-    |> Seq.filter (fun dri ->
-        dri.MovingAverage50Days < dri.MovingAverage200Days
-        && dri.Stock.OpenValue > dri.PreviousDay.CloseValue
-        && dri.Previous50DayStandardDeviation > (float (dri.MovingAverage50Days / 100m) * 10.)
-        && dri.Previous200DayStandardDeviation > (float (dri.MovingAverage200Days / 100m) * 10.)
-        && dri.Previous50DayLow > dri.Previous200DayLow
-        && dri.Stock.OpenValue < ((dri.MovingAverage200Days / 100m) * 90m)
-    //&& dri.Previous50DayHigh > dri.Previous200DayHigh
-    )
-    |> List.ofSeq
 
 // Stable -
 // 50 day high and low diff -
 // 200 day high and low diff -
-
-
-
-(*
-List.init 2500 (fun i -> i * 10000)
-|> List.map (fun v -> v, 10000)
-|> List.iter (fun (offset, limit) ->
-
-    Persistence.Operations.selectStockRecords ctx [ "LIMIT @0 OFFSET @1" ] [ limit; offset ]
-    |> List.iter (fun sr ->
-        ({ Symbol = sr.Symbol
-           EntryDate = sr.EntryDate
-           MovingAverage50Days =
-             Store.previousXDays ctx 50 sr.EntryDate sr.Symbol
-             |> List.averageBy (fun s -> s.CloseValue)
-           MovingAverage200Days =
-             Store.previousXDays ctx 200 sr.EntryDate sr.Symbol
-             |> List.averageBy (fun s -> s.CloseValue) }: Persistence.Parameters.NewMovingAverage)
-        |> Operations.insertMovingAverage ctx)
-
-    ctx.ExecuteInTransaction (fun t ->
-        //let records =
-        let records =
-            Persistence.Operations.selectStockRecords t [ "LIMIT @0 OFFSET @1" ] [ limit; offset ]
-
-        records
-        |> List.iteri (fun i sr ->
-            ({ Symbol = sr.Symbol
-               EntryDate = sr.EntryDate
-               MovingAverage50Days =
-                 Store.previousXDays t 50 sr.EntryDate sr.Symbol
-                 |> List.averageBy (fun s -> s.CloseValue)
-               MovingAverage200Days =
-                 Store.previousXDays t 200 sr.EntryDate sr.Symbol
-                 |> List.averageBy (fun s -> s.CloseValue) }: Persistence.Parameters.NewMovingAverage)
-            |> Operations.insertMovingAverage t
-
-            printfn $"{i}/{records.Length} added.")
-
-        printfn $"{offset} ({limit}) selected")
-    |> fun r ->
-        match r with
-        | Ok _ -> printfn "Added."
-        | Error e -> printfn $"Error: {e}")
-*)
-
-//let allRecords = Persistence.Operations.selectStockRecords ctx [ "LIMIT 10000 OFFSET 10000" ] []
-
-Persistence.Operations.selectStockRecords
-    ctx
-    [ "WHERE symbol = @0 AND Date(entry_date) >= DATE(@1) AND DATE(entry_date) < DATE(@2)" ]
-    [ "TSLA"
-      DateTime(2020, 1, 1)
-      DateTime(2020, 2, 1) ]
-|> List.map (fun s ->
-    ({ Date = s.EntryDate
-       Open = s.OpenValue
-       Close = s.CloseValue
-       High = s.HighValue
-       Low = s.LowValue
-       AdjClose = s.AdjustedCloseValue
-       Volume = s.VolumeValue
-       MovingAverage200 = 0m
-       MovingAverage50 = 0m }: DataPoint))
-
-
-(*
-[ ({ Date = DateTime(2020, 1, 1)
-     Open = 10m
-     Close = 10m
-     High = 15m
-     Low = 10m
-     AdjClose = 10m
-     Volume = 100m }: DataPoint)
-  ({ Date = DateTime(2020, 2, 1)
-     Open = 12.5m
-     Close = 12.5m
-     High = 15m
-     Low = 10m
-     AdjClose = 10m
-     Volume = 100m }: DataPoint)
-  ({ Date = DateTime(2020, 3, 1)
-     Open = 15m
-     Close = 15m
-     High = 17.5m
-     Low = 12.5m
-     AdjClose = 15m
-     Volume = 100m }: DataPoint)
-  ({ Date = DateTime(2020, 4, 1)
-     Open = 17.5m
-     Close = 17.5m
-     High = 20m
-     Low = 15m
-     AdjClose = 20m
-     Volume = 100m }: DataPoint)
-  ({ Date = DateTime(2020, 5, 1)
-     Open = 20m
-     Close = 20m
-     High = 20m
-     Low = 17.5m
-     AdjClose = 20m
-     Volume = 100m }: DataPoint) ]
-*)
-|> generateCandleStickChart ({ ValuePaddingPercent = 20m })
-|> saveToFile "C:\\ProjectData\\fstock\\test_chart.svg" 120 100
 
 //Import.build Import.logToConsole "E:\\data\\stock_market"
 
