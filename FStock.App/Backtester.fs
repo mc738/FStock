959a8928db9ac8040459ef477860a3001395b1b8
Max Clifford <me@mclifford.dev>
2024-01-20 13:08:38 +0000

    Added Backtester.fs

---
+++ FStock.App/Backtester.fs
@@ -0,0 +1,26 @@
+﻿namespace FStock.App
+
+open Freql.Sqlite
+
+module Backtester =
+
+    module V1 =
+
+        open FStock.Modeling.V1
+        open FStock.Modeling.V1.Backtesting
+
+        let run (storePath: string) =
+            
+            use context = SqliteContext()
+
+            let testingCtx =
+                ({ Settings =
+                    { BuyValueMode = ValueMode.Open
+                      SellValueMode = ValueMode.High
+                      ActionCombinationMode = ActionCombinationMode.Simple }
+                   CurrentPositionHandler = failwith "todo"
+                   HistoricPositionsHandler = fun _ -> []
+                   NewStateRewriters = [] }
+                : TestingContext)
+
+            ()
