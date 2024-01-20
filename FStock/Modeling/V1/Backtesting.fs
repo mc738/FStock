618ccc38248e4713c587ba26b82105e8ce2f796e
Max Clifford <me@mclifford.dev>
2024-01-20 14:01:51 +0000

    Updated Backtesting.fs to handled copying behaviours

--- FStock/Modeling/V1/Backtesting.fs
+++ FStock/Modeling/V1/Backtesting.fs
@@ -22,39 +22,76 @@ module Backtesting =
               Logs = [] }
 
         member us.Buy(symbol, date, price, volume) =
-            { us with
-                Portfolio = us.Portfolio.Buy(symbol, date, price, volume)
-                Logs = us.Logs @ [ "" ] }
+            match us.Portfolio.TryBuy(symbol, date, price, volume) with
+            | BuyResult.Success(newPortfolio, newId) ->
+                { us with
+                    Portfolio = newPortfolio
+                    Logs = us.Logs @ [ $"Brought {volume} at {price} of {symbol}" ] }
+                |> fun ns -> UpdateStateResult.Success(ns, Some newId)
+            | BuyResult.Failure message ->
+                { us with
+                    Logs =
+                        us.Logs
+                        @ [ $"Failed to buy {volume} at {price} of {symbol}. Result: {message}" ] }
+                |> fun ns -> UpdateStateResult.Failure(ns, message)
 
         member us.Sell(symbol, date, price, volume) =
 
             match us.Portfolio.TrySell(symbol, date, price, volume) with
-            | Ok np ->
+            | SellResult.Success(newPortfolio, newId) ->
                 { us with
-                    Portfolio = np
-                    Logs = us.Logs @ [ "" ] }
-            | Error e -> { us with Logs = us.Logs @ [ "" ] }
+                    Portfolio = newPortfolio
+                    Logs = us.Logs @ [ $"Sold {volume} at {price} of {symbol}" ] }
+                |> fun ns -> UpdateStateResult.Success(ns, newId)
+            | SellResult.Failure message ->
+                { us with
+                    Logs =
+                        us.Logs
+                        @ [ $"Failed to buy {volume} at {price} of {symbol}. Result: {message}" ] }
+                |> fun ns -> UpdateStateResult.Failure(ns, message)
+
+    and [<RequireQualifiedAccess>] UpdateStateResult =
+        | Success of NewState: UpdateState * NewPositionId: string option
+        | Failure of NewState: UpdateState * Message: string
 
     type NewStateRewriter = UpdateState -> UpdateState
 
     type TestingSettings =
-        {
-            BuyValueMode: ValueMode
-            /// <summary>
-            /// The ValueModel to be used when checking conditions.
-            /// This is separate to help model situations where a stop-lose might be hit within a period.
-            /// By setting this to ValueMode.Low you can ensure conditions are being checked against the low value
-            /// for that period.  
-            /// </summary>
-            ConditionTestValueMode: ValueMode
-            SellValueMode: ValueMode
-            ActionCombinationMode: ActionCombinationMode
-        }
-    
+        { BuyValueMode: ValueMode
+          SellValueMode: ValueMode
+          ActionCombinationMode: ActionCombinationMode }
+
     type TestingContext =
         { Settings: TestingSettings
-          CurrentPositionHandler: OpenPosition -> DateTime -> CurrentPosition
+          CurrentPositionHandler: OpenPosition -> DateTime -> CurrentPosition option
+          HistoricPositionsHandler: HistoricPositionFilter -> HistoricPosition list
           NewStateRewriters: NewStateRewriter list }
+
+    let testCondition
+        (ctx: TestingContext)
+        (position: OpenPosition)
+        (current: CurrentPosition)
+        (condition: PositionCondition)
+        =
+        let rec handle (condition: PositionCondition) =
+            match condition with
+            | PercentageGrowth(percent, valueMapper) ->
+                ((valueMapper.GetValue current - position.BuyPrice) / (position.BuyPrice) * 100m)
+                >= percent
+            | FixedValue(value, valueMapper) -> valueMapper.GetValue current >= value
+            | PercentageLoss(percent, valueMapper) ->
+                (((position.BuyPrice - valueMapper.GetValue current) / position.BuyPrice) * 100m)
+                >= percent
+            | FixedLoss(value, valueMapper) -> valueMapper.GetValue current <= value
+            | Duration length -> (current.Date - position.Start).Days >= length
+            | Bespoke handler -> handler position current ctx.HistoricPositionsHandler
+            | And(a, b) -> handle a && handle b
+            | Or(a, b) -> handle a || handle b
+            | All positionConditions -> positionConditions |> List.exists (fun c -> handle c |> not) |> not
+            | Any positionConditions -> positionConditions |> List.exists handle
+            | Not positionCondition -> handle positionCondition |> not
+
+        handle condition
 
     let fetchBehaviours (state: ModelState) (position: OpenPosition) =
         state.BehaviourMaps
@@ -73,36 +110,38 @@ module Backtesting =
         (behaviours: PrioritizedBehaviour list)
         =
         // Get the current position.
-        let cp = ctx.CurrentPositionHandler position date
+        ctx.CurrentPositionHandler position date
 
-        // Now prioritize behaviours.
+        |> Option.bind (fun cp ->
+            // Now prioritize behaviours.
 
-        // Then run through them until one is triggered.
-        behaviours
-        |> List.sortBy (fun pb -> pb.Priority)
-        |> List.fold
-            (fun (pa: PrioritizedActions list) pb ->
-                match pb.Behaviour.Condition.Test(position, cp, ctx.Settings.ConditionTestValueMode) with
-                | true ->
-                    pa
-                    @ [ { Actions = pb.Behaviour.Actions
-                          Priority = pb.Priority } ]
-                | false -> pa)
-            []
-        |> fun pas ->
-            // Combine actions so they are simplified.
-            // For example if there is action to buy 10 and sell 5 then this will be combined to buy 5.
-            match ctx.Settings.ActionCombinationMode with
-            | ActionCombinationMode.Simple ->
-                // Simple mode -> first actions hit
-                match pas.IsEmpty |> not with
-                | true -> pas.Head.Actions
-                | false -> []
-            | ActionCombinationMode.Bespoke fn -> fn pas
-        |> fun ta ->
-            { Position = position
-              Actions = ta
-              CurrentPosition = cp }
+            // Then run through them until one is triggered.
+            behaviours
+            |> List.sortBy (fun pb -> pb.Priority)
+            |> List.fold
+                (fun (pa: PrioritizedActions list) pb ->
+                    match testCondition ctx position cp pb.Behaviour.Condition with
+                    | true ->
+                        pa
+                        @ [ { Actions = pb.Behaviour.Actions
+                              Priority = pb.Priority } ]
+                    | false -> pa)
+                []
+            |> fun pas ->
+                // Combine actions so they are simplified.
+                // For example if there is action to buy 10 and sell 5 then this will be combined to buy 5.
+                match ctx.Settings.ActionCombinationMode with
+                | ActionCombinationMode.Simple ->
+                    // Simple mode -> first actions hit
+                    match pas.IsEmpty |> not with
+                    | true -> pas.Head.Actions
+                    | false -> []
+                | ActionCombinationMode.Bespoke fn -> fn pas
+            |> fun ta ->
+                { Position = position
+                  Actions = ta
+                  CurrentPosition = cp }
+                |> Some)
 
     let handleActions (ctx: TestingContext) (state: ModelState) (date: DateTime) (actions: TriggeredActions list) =
         actions
@@ -113,33 +152,92 @@ module Backtesting =
                     (fun (us: UpdateState) a ->
                         match a with
                         | PositionAction.IncreasePositionByFixedAmount amount ->
-                            us.Buy(ta.Position.Symbol, date, getValue ta.CurrentPosition, amount)
+                            match
+                                us.Buy(
+                                    ta.Position.Symbol,
+                                    date,
+                                    ta.CurrentPosition.GetValue ctx.Settings.BuyValueMode,
+                                    amount
+                                )
+                            with
+                            | UpdateStateResult.Success(ns, newPositionId) ->
+                                // TODO add behaviours to new position
+
+                                ns
+                            | UpdateStateResult.Failure(ns, msg) -> ns
                         | PositionAction.IncreasePositionByPercentage percent ->
                             let volumeIncrease = (ta.Position.Volume / 100m) * percent
 
-                            us.Buy(ta.Position.Symbol, date, getValue ta.CurrentPosition, volumeIncrease)
+                            match
+                                us.Buy(
+                                    ta.Position.Symbol,
+                                    date,
+                                    ta.CurrentPosition.GetValue ctx.Settings.BuyValueMode,
+                                    volumeIncrease
+                                )
+                            with
+                            | UpdateStateResult.Success(ns, newPositionId) ->
+                                // TODO add behaviours to new position
+
+                                ns
+                            | UpdateStateResult.Failure(ns, msg) -> ns
                         | PositionAction.DecreasePositionByFixedAmount amount ->
                             // TODO pass on action mappings to new position.
-                            us.Sell(ta.Position.Id, date, getValue ta.CurrentPosition, amount)
+                            match
+                                us.Sell(
+                                    ta.Position.Id,
+                                    date,
+                                    ta.CurrentPosition.GetValue ctx.Settings.SellValueMode,
+                                    amount
+                                )
+                            with
+                            | UpdateStateResult.Success(ns, newPositionId) ->
+                                // Copy across behaviours for new position.
+                                match newPositionId with
+                                | Some npi ->
+                                    ns.BehaviourMaps
+                                    |> List.filter (fun bm -> bm.PositionId = ta.Position.Id)
+                                    |> List.map (fun bm -> { bm with PositionId = npi })
+                                    |> fun nbm ->
+                                        { ns with
+                                            BehaviourMaps = ns.BehaviourMaps @ nbm }
+                                | None -> ns
+                            | UpdateStateResult.Failure(ns, msg) -> ns
                         | PositionAction.DecreasePositionByPercentage percent ->
                             let volumeDecrease = (ta.Position.Volume / 100m) * percent
-                            us.Sell(ta.Position.Id, date, getValue ta.CurrentPosition, volumeDecrease))
+
+                            match
+                                us.Sell(
+                                    ta.Position.Id,
+                                    date,
+                                    ta.CurrentPosition.GetValue ctx.Settings.BuyValueMode,
+                                    volumeDecrease
+                                )
+                            with
+                            | UpdateStateResult.Success(ns, newPositionId) ->
+                                match newPositionId with
+                                | Some npi ->
+                                    ns.BehaviourMaps
+                                    |> List.filter (fun bm -> bm.PositionId = ta.Position.Id)
+                                    |> List.map (fun bm -> { bm with PositionId = npi })
+                                    |> fun nbm ->
+                                        { ns with
+                                            BehaviourMaps = ns.BehaviourMaps @ nbm }
+                                | None -> ns
+                            | UpdateStateResult.Failure(ns, msg) -> ns)
                     state)
             (UpdateState.Create(state.Portfolio, state.BehaviourMaps))
 
     let rewriteState (ctx: TestingContext) (state: UpdateState) =
         ctx.NewStateRewriters |> List.fold (fun ns r -> r ns) state
 
-    let progressState (ctx: TestingContext) (state: ModelState) (date: DateTime) =
+    let progressState (ctx: TestingContext) (date: DateTime) (state: ModelState) =
         state.Portfolio.OpenPositions
         |> List.map (fun op -> op, fetchBehaviours state op)
-        |> List.map (fun (op, pbs) -> handleBehaviours ctx date op pbs)
+        |> List.choose (fun (op, pbs) -> handleBehaviours ctx date op pbs)
         |> (handleActions ctx state date)
         |> rewriteState ctx
-
-
-
-
-
-
-    ()
+        |> fun ns ->
+            { state with
+                Portfolio = ns.Portfolio
+                BehaviourMaps = ns.BehaviourMaps }
