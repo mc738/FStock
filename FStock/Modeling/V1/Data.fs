7d08b504bd6dfb21348584bbec238964001d8de3
Max Clifford <me@mclifford.dev>
2024-01-20 15:37:55 +0000

    Added Queries

--- FStock/Modeling/V1/Data.fs
+++ FStock/Modeling/V1/Data.fs
@@ -1,6 +1,27 @@
 ﻿namespace FStock.Modeling.V1
 
+open System
+open FStock.Data
+open FStock.Data.Store
+open Freql.Sqlite
+
+[<RequireQualifiedAccess>]
 module Data =
     
-    let getCurrentPosition ()
+    let getCurrentPosition (store: FStockStore) (openPosition: OpenPosition) (date: DateTime) =
+        // TODO add cache?
+        store.GetStockForDate(openPosition.Symbol, date)
+        |> Option.map (fun s ->
+            ({ Symbol = s.Symbol
+               Date = s.EntryDate
+               Open = s.OpenValue
+               High = s.HighValue
+               Low = s.LowValue
+               Close = s.CloseValue
+               AdjustedClose = s.AdjustedCloseValue }
+            : CurrentPosition))
 
+    let getHistoricPositions (store: FStockStore) (filter: HistoricPositionFilter) : HistoricPosition list =
+        // TODO add cache.
+        
+        []
