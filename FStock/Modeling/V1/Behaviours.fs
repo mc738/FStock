ee5df51434270f36caa041d2236e8a8b2aaecab2
Max Clifford <me@mclifford.dev>
2024-01-22 21:57:47 +0000

    Work on Behaviours.fs

--- FStock/Modeling/V1/Behaviours.fs
+++ FStock/Modeling/V1/Behaviours.fs
@@ -12,7 +12,6 @@ module Behaviours =
             ({ Condition = Conditions.``percentage take profit based on high value`` 10m
                Actions = [ PositionAction.IncreasePositionByPercentage(10m, InvestmentType.LiquidityOnly) ] }
             : PositionBehaviour)
-            
         
         let ``take profit at 20%`` =
             ({ Condition = Conditions.``percentage take profit based on high value`` 20m
@@ -21,6 +20,11 @@ module Behaviours =
         let all = [ ``matched 10% reinvestment in winning position`` ]
 
     module Pessimistic =
+        
+        
+        let ``10% stop-loss`` =
+            ({ Condition = Conditions.``percentage stop-loss based on low value`` 10m
+               Actions = [ PositionAction.DecreasePositionByFixedAmount 100m ] })
 
         let all = []
 
