ac6707d8b3555dc86a561e73c4cb67627909d45b
Max Clifford <me@mclifford.dev>
2024-01-23 20:37:30 +0000

    Created version on RelativeStrengthIndex.fs

--- FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
+++ FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
@@ -1,6 +1,7 @@
 ﻿namespace FStock.Analysis.V1.TechnicalIndicators
 
 open System
+open System.Collections.Generic
 open FStock.Analysis.V1.Persistence
 
 [<RequireQualifiedAccess>]
@@ -24,13 +25,15 @@ module RelativeStrengthIndex =
 
     type CalculationState =
         { I: int
-          Items: RsiItem list
-          Gains: decimal list
-          Losses: decimal list }
+          Items: RsiItem list }
 
-        static member Empty() = { Items = []; Gains = []; Losses = [] }
+        static member Empty() = { I = 0; Items = [] }
 
     let calculate (parameters: Parameters) (values: InputItem list) =
+        let gains = ResizeArray()
+        let gains = Queue<decimal>()
+        let losses = Queue<decimal>()
+
         values
         |> List.fold
             (fun (state: CalculationState) v ->
@@ -59,35 +62,74 @@ module RelativeStrengthIndex =
                     let difference = v.Price - prevItem.Close
 
 
-                    let (newGains, newLosses) =
+
+                    let (gain, loss) =
                         match difference with
-                        | d when d > 0m -> difference :: state.Gains, 0m :: state.Losses
-                        | d when d < 0m -> 0m :: state.Gains, difference :: state.Losses
-                        | _ -> 0m :: state.Gains, 0m :: state.Losses
+                        | d when d > 0m ->
+                            gains.Enqueue(d)
+                            losses.Enqueue(0)
+                            d, 0m
 
-                    match state.I with
-                    | i when i < parameters.Periods ->
-                        {
-                            
-                            
-                        }
-                    | i when i = parameters.Periods -> ()
-                    | _ -> ()
+                        | d when d < 0m ->
+                            gains.Enqueue(0)
+                            losses.Enqueue(d)
+                            0m, d
+                        | _ ->
+                            gains.Enqueue(0)
+                            losses.Enqueue(0)
+                            0m, 0m
 
+                    let newItem =
+                        match state.I with
+                        | i when i < parameters.Periods ->
+                            { Date = v.Date
+                              Close = v.Price
+                              Gain = gain
+                              Loss = loss
+                              AverageGain = 0m
+                              AverageLoss = 0m
+                              Rsi = 0m }
+                        | i when i = parameters.Periods ->
+                            let avgGain = Seq.sum gains / (decimal gains.Count)
+                            let avgLoss = Seq.sum losses / (decimal losses.Count)
 
+                            let rs = avgGain / avgLoss
 
-                    { state })
-            (CalculationState.Empty(), 0)
+                            { Date = v.Date
+                              Close = v.Price
+                              Gain = gain
+                              Loss = loss
+                              AverageGain = avgGain
+                              AverageLoss = avgLoss
+                              Rsi = 100m - (100m / (1m + rs)) }
+                        | _ ->
+                            let avgGain =
+                                (prevItem.AverageGain * decimal (parameters.Periods - 1) + gain)
+                                / decimal parameters.Periods
 
-    let calculate _ =
+                            let avgLoss =
+                                (prevItem.AverageLoss * decimal (parameters.Periods - 1) + loss)
+                                / decimal parameters.Periods
 
-        let averageGain = 0m
-        let averageLoss = 0m
+                            let rs = avgGain / avgLoss
 
+                            { Date = v.Date
+                              Close = v.Price
+                              Gain = gain
+                              Loss = loss
+                              AverageGain = avgGain
+                              AverageLoss = avgLoss
+                              Rsi = 100m - (100m / (1m + rs)) }
 
+                    // If i is bigger than periods, progress the window by popping last items in gains and losses.
+                    if state.I >= parameters.Periods then
+                        gains.Dequeue |> ignore
+                        losses.Dequeue |> ignore
 
-
-        ()
+                    { state with
+                        I = state.I + 1
+                        Items = newItem :: state.Items })
+            (CalculationState.Empty())
 
 
 
