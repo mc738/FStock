9e24e72b049780fadf23eb3b404cf0327019e3d9
Max Clifford <me@mclifford.dev>
2024-01-23 18:56:14 +0000

    Added RelativeStrengthIndex.fs

---
+++ FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
@@ -0,0 +1,18 @@
+﻿namespace FStock.Analysis.V1.TechnicalIndicators
+
+module RelativeStrengthIndex =
+    
+    let calculate _ =
+        
+        let averageGain = 0m
+        let averageLoss = 0m
+        
+        
+        
+        
+        ()
+    
+    
+    
+    ()
+
