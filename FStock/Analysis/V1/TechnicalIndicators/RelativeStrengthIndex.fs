383e99d99f77cf61f30ade316f6c0db4ca38abc6
Max Clifford <me@mclifford.dev>
2024-01-23 19:31:20 +0000

    Work on RelativeStrengthIndex.fs calculation

--- FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
+++ FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
@@ -1,18 +1,42 @@
 ﻿namespace FStock.Analysis.V1.TechnicalIndicators
 
+[<RequireQualifiedAccess>]
 module RelativeStrengthIndex =
+
+    type Parameters =
+        { Periods: int }
+
+        static member Default() = { Periods = 14 }
+
+    
+    type Step1Item =
+        {
+            PeriodNumber: int
+            Price: decimal
+            Difference: decimal
+            Gain: decimal
+            Loss: decimal
+        }
+    
+    let step1 (values: decimal list) =
+        values
+        |> List.fold (fun (prevValue, r) v ->
+            match prevValue with
+            | None -> 0m
+            | Some -> 
+            
+             ) (None, [])
     
     let calculate _ =
-        
+
         let averageGain = 0m
         let averageLoss = 0m
-        
-        
-        
-        
-        ()
-    
-    
-    
-    ()
 
+
+
+
+        ()
+
+
+
+    ()
