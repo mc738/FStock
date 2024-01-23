be111af9181377bd3a125139ee5b8bac792a1a29
Max Clifford <me@mclifford.dev>
2024-01-23 19:58:05 +0000

    Work on RelativeStrengthIndex.fs

--- FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
+++ FStock/Analysis/V1/TechnicalIndicators/RelativeStrengthIndex.fs
@@ -1,5 +1,8 @@
 ﻿namespace FStock.Analysis.V1.TechnicalIndicators
 
+open System
+open FStock.Analysis.V1.Persistence
+
 [<RequireQualifiedAccess>]
 module RelativeStrengthIndex =
 
@@ -8,25 +11,74 @@ module RelativeStrengthIndex =
 
         static member Default() = { Periods = 14 }
 
-    
-    type Step1Item =
-        {
-            PeriodNumber: int
-            Price: decimal
-            Difference: decimal
-            Gain: decimal
-            Loss: decimal
-        }
-    
-    let step1 (values: decimal list) =
+    type InputItem = { Date: DateTime; Price: decimal }
+
+    type RsiItem =
+        { Date: DateTime
+          Close: decimal
+          Gain: decimal
+          Loss: decimal
+          AverageGain: decimal
+          AverageLoss: decimal
+          Rsi: decimal }
+
+    type CalculationState =
+        { I: int
+          Items: RsiItem list
+          Gains: decimal list
+          Losses: decimal list }
+
+        static member Empty() = { Items = []; Gains = []; Losses = [] }
+
+    let calculate (parameters: Parameters) (values: InputItem list) =
         values
-        |> List.fold (fun (prevValue, r) v ->
-            match prevValue with
-            | None -> 0m
-            | Some -> 
-            
-             ) (None, [])
-    
+        |> List.fold
+            (fun (state: CalculationState) v ->
+                match state.Items.IsEmpty with
+                | true ->
+                    let item =
+                        { Date = v.Date
+                          Close = v.Price
+                          Gain = 0m
+                          Loss = 0m
+                          AverageGain = 0m
+                          AverageLoss = 0m
+                          Rsi = 0m }
+
+                    { state with
+                        Items = item :: state.Items
+                        I = state.I + 1 }
+
+                //(Some item, item :: acc)
+                | false ->
+                    // Because the items are currently in reverse order we can just grab the head to get the last one,
+                    let prevItem = state.Items.Head
+
+
+
+                    let difference = v.Price - prevItem.Close
+
+
+                    let (newGains, newLosses) =
+                        match difference with
+                        | d when d > 0m -> difference :: state.Gains, 0m :: state.Losses
+                        | d when d < 0m -> 0m :: state.Gains, difference :: state.Losses
+                        | _ -> 0m :: state.Gains, 0m :: state.Losses
+
+                    match state.I with
+                    | i when i < parameters.Periods ->
+                        {
+                            
+                            
+                        }
+                    | i when i = parameters.Periods -> ()
+                    | _ -> ()
+
+
+
+                    { state })
+            (CalculationState.Empty(), 0)
+
     let calculate _ =
 
         let averageGain = 0m
