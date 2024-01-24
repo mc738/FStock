ecbd04b80b5003f3a0d0a575a60f2ff7d1ef8f44
Max Clifford <me@mclifford.dev>
2024-01-24 19:49:15 +0000

    Added Discardable field

--- FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
+++ FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
@@ -11,7 +11,13 @@ module ExponentialMovingAverage =
     type EmaItem =
         { Date: DateTime
           Value: decimal
-          Ema: decimal }
+          Ema: decimal
+          /// <summary>
+          /// Specifies if the value can be discarded.
+          /// This normally means the item predates the first calculable entry and thus has no EMA value.
+          /// It also means the entry might not be used for further analysis. 
+          /// </summary>
+          Discardable: bool }
 
     type CalculationState =
         { I: int
@@ -29,19 +35,22 @@ module ExponentialMovingAverage =
                         // If less than window size - 1 there is no EMA to be calculated.
                         { Date = v.Date
                           Value = v.Price
-                          Ema = 0m }
+                          Ema = 0m
+                          Discardable = true }
                     | i when i = (parameters.WindowSize - 1) ->
                         // If equal to window size - 1 use SMA.
                         { Date = v.Date
                           Value = v.Price
-                          Ema = state.Items |> List.averageBy (fun i -> i.Value) }
+                          Ema = state.Items |> List.averageBy (fun i -> i.Value)
+                          Discardable = false }
                     | _ ->
                         { Date = v.Date
                           Value = v.Price
                           Ema =
                             (v.Price * (parameters.Smoothing / decimal (1 + parameters.WindowSize)))
                             + state.Items.Head.Ema
-                              * (1m - (parameters.Smoothing / decimal (1 + parameters.WindowSize))) }
+                              * (1m - (parameters.Smoothing / decimal (1 + parameters.WindowSize)))
+                          Discardable = false }
 
                 { state with
                     I = state.I + 1
