da3b093c4dbd626baa827983ca841718bd3ca256
Max Clifford <me@mclifford.dev>
2024-01-24 19:15:17 +0000

    Work on ExponentialMovingAverage.fs

--- FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
+++ FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
@@ -1,6 +1,21 @@
 ﻿namespace FStock.Analysis.V1.TechnicalIndicators
 
-module ExponentialMovingAverage =
-    
-    ()
+open System
 
+module ExponentialMovingAverage =
+
+    type Parameters = { WindowSize: int; Smoothing: int }
+
+    type InputItem = { Date: DateTime; Price: decimal }
+    
+    type EmaItem =
+        { Date: DateTime
+          Value: decimal
+          Ema: decimal }
+
+    type CalculationState =
+        { I: int
+          Items: EmaItem list }
+    
+    let calculate =
+        ()
