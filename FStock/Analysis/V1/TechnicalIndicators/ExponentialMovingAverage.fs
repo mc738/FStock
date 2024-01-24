bffd7f9b6c6bd9ed0944fb3cb92295ab0629cb83
Max Clifford <me@mclifford.dev>
2024-01-24 19:26:54 +0000

    Added ExponentialMovingAverage.fs calculation

--- FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
+++ FStock/Analysis/V1/TechnicalIndicators/ExponentialMovingAverage.fs
@@ -4,10 +4,10 @@ open System
 
 module ExponentialMovingAverage =
 
-    type Parameters = { WindowSize: int; Smoothing: int }
+    type Parameters = { WindowSize: int; Smoothing: decimal }
 
     type InputItem = { Date: DateTime; Price: decimal }
-    
+
     type EmaItem =
         { Date: DateTime
           Value: decimal
@@ -16,6 +16,34 @@ module ExponentialMovingAverage =
     type CalculationState =
         { I: int
           Items: EmaItem list }
-    
-    let calculate =
-        ()
+
+        static member Empty() = { I = 0; Items = [] }
+
+    let calculate (parameters: Parameters) (values: InputItem list) =
+        values
+        |> List.fold
+            (fun (state: CalculationState) v ->
+                let newItem =
+                    match state.I with
+                    | i when i < (parameters.WindowSize - 1) ->
+                        // If less than window size - 1 there is no EMA to be calculated.
+                        { Date = v.Date
+                          Value = v.Price
+                          Ema = 0m }
+                    | i when i = (parameters.WindowSize - 1) ->
+                        // If equal to window size - 1 use SMA.
+                        { Date = v.Date
+                          Value = v.Price
+                          Ema = state.Items |> List.averageBy (fun i -> i.Value) }
+                    | _ ->
+                        { Date = v.Date
+                          Value = v.Price
+                          Ema =
+                            (v.Price * (parameters.Smoothing / decimal (1 + parameters.WindowSize)))
+                            + state.Items.Head.Ema
+                              * (1m - (parameters.Smoothing / decimal (1 + parameters.WindowSize))) }
+
+                { state with
+                    I = state.I + 1
+                    Items = newItem :: state.Items })
+            (CalculationState.Empty())
