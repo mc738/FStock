dd957372f30361fb13fb7347769a5b9e70164294
Max Clifford <me@mclifford.dev>
2024-01-25 19:12:55 +0000

    Fixed up SimpleMovingAverage.fs

--- FStock/Analysis/V1/TechnicalIndicators/SimpleMovingAverage.fs
+++ FStock/Analysis/V1/TechnicalIndicators/SimpleMovingAverage.fs
@@ -1,7 +1,59 @@
 ﻿namespace FStock.Analysis.V1.TechnicalIndicators
 
+open FStock.Analysis.V1.Core
+
 [<RequireQualifiedAccess>]
 module SimpleMovingAverage =
     
-    ()
+    open System
+    open FStock.Analysis.V1.Core
 
+    type Parameters = { WindowSize: int }
+
+    type SmaItem =
+        {
+            Date: DateTime
+            Value: decimal
+            Sma: decimal
+            /// <summary>
+            /// Specifies if the value can be discarded.
+            /// This normally means the item predates the first calculable entry and thus has no SMA value.
+            /// It also means the entry might not be used for further analysis.
+            /// </summary>
+            Discardable: bool
+        }
+
+    type CalculationState =
+        { I: int
+          Items: SmaItem list }
+
+        static member Empty() = { I = 0; Items = [] }
+
+    let calculate (parameters: Parameters) (values: BasicInputItem list) =
+        values
+        |> List.fold
+            (fun (state: CalculationState) v ->
+                let newItem =
+                    match state.I with
+                    | i when i < (parameters.WindowSize - 1) ->
+                        // If less than window size - 1 there is no EMA to be calculated.
+                        { Date = v.Date
+                          Value = v.Price
+                          Sma = 0m
+                          Discardable = true }
+                    | _ ->
+                        // If equal to window size - 1 use SMA.
+                        { Date = v.Date
+                          Value = v.Price
+                          Sma =
+                            state.Items
+                            |> List.take (parameters.WindowSize - 1)
+                            |> List.map (fun i -> i.Value)
+                            |> prepend v.Price 
+                            |> List.average
+                          Discardable = false }
+
+                { state with
+                    I = state.I + 1
+                    Items = newItem :: state.Items })
+            (CalculationState.Empty())
