f4e9e8c552fdbc074160019a27ed9b798b7a87b9
Max Clifford <me@mclifford.dev>
2024-01-26 19:59:59 +0000

    Added VolumeChart.fs

---
+++ FStock/Analysis/V1/Visualizations/Charts/VolumeChart.fs
@@ -0,0 +1,45 @@
+﻿namespace FStock.Analysis.V1.Visualizations.Charts
+
+[<RequireQualifiedAccess>]
+module VolumeChart =
+
+    open FSVG
+
+    type Parameters =
+        { MinimumX: float
+          MaximumX: float
+          MinimumY: float
+          MaximumY: float
+          LeftYAxis: bool
+          RightYAxis: bool
+          XAxisStartOverride: float option
+          XAxisEndOverride: float option
+          AxisStyle: Style }
+
+    let create (parameters: Parameters) =
+        [ Line
+              { X1 = parameters.MinimumX
+                X2 = parameters.MinimumX
+                Y1 = parameters.MinimumY
+                Y2 = parameters.MaximumY
+                Style = parameters.AxisStyle }
+          Line
+              { X1 = parameters.MaximumX
+                X2 = parameters.MaximumX
+                Y1 = parameters.MinimumY
+                Y2 = parameters.MaximumY
+                Style = parameters.AxisStyle }
+          Line
+              { X1 = parameters.XAxisStartOverride |> Option.defaultValue parameters.MinimumX
+                X2 = parameters.XAxisEndOverride |> Option.defaultValue parameters.MaximumX
+                Y1 = parameters.MaximumY
+                Y2 = parameters.MaximumY
+                Style = parameters.AxisStyle }
+
+          // Test
+          Line
+              { X1 = parameters.MinimumX
+                X2 = parameters.MaximumX
+                Y1 = normalizeYValue 25m 0m 100m parameters.MinimumY parameters.MaximumY true
+                Y2 = normalizeYValue 75m 0m 100m parameters.MinimumY parameters.MaximumY true
+                Style = parameters.AxisStyle } ]
