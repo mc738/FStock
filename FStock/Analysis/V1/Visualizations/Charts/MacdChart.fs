4f7d6fe7dcb5b67eebc8b030911ee4052259f55e
Max Clifford <me@mclifford.dev>
2024-01-28 21:22:55 +0000

    Added histograms to MacdChart.fs

--- FStock/Analysis/V1/Visualizations/Charts/MacdChart.fs
+++ FStock/Analysis/V1/Visualizations/Charts/MacdChart.fs
@@ -1,6 +1,7 @@
 ﻿namespace FStock.Analysis.V1.Visualizations.Charts
 
 open FStock.Analysis.V1.Core
+open FStock.Analysis.V1.Persistence
 open FStock.Analysis.V1.TechnicalIndicators
 
 [<RequireQualifiedAccess>]
@@ -20,36 +21,69 @@ module MacdChart =
           AxisStyle: Style
           Data: StockData }
 
-    let createMcadLine (parameters: Parameters) (data: MovingAverageConvergenceDivergence.MacdItem list) =
+    let createHistograms (parameters: Parameters) (minValue: decimal) (maxValue: decimal) (data: MovingAverageConvergenceDivergence.MacdItem list) =
+        
+        let sectionPadding = 0.5
+
+        let sectionWidth =
+            (parameters.MaximumX - parameters.MinimumX)
+            / float parameters.Data.BaseData.Length
+
+        let barWidth = sectionWidth - (sectionPadding * 2.)
+
+        let barStyle =
+            { Style.Default() with
+                Opacity = Some 1.
+                Fill = Some "blue" }
+        
+        data
+        |> List.mapi (fun i d ->
+            
+            let prevHValue =
+                match i > 0 with
+                | true -> data.Item (i - 1) |> fun pv -> pv.MacdValue - pv.SignalEma
+                | false -> 0m
+                
+            let hValue = d.MacdValue - d.SignalEma
+            
+            let opacity =
+                match hValue > 0m, prevHValue > 0m, hValue >= prevHValue with
+                | true, true, true -> 1.
+                | true, true, false -> 0.5
+                | false, false, false -> 1.
+                | false, false, true -> 0.5
+                | true, false, _ -> 1.
+                | false, true, _ -> 1.
+                
+            match hValue > 0m with
+            | true ->
+                createBar
+                    hValue
+                    0m
+                    maxValue
+                    parameters.MinimumY
+                    (parameters.MinimumY + ((parameters.MaximumY - parameters.MinimumY) / 2.))
+                    (parameters.MinimumX + (sectionWidth * float i) + sectionPadding)
+                    barWidth
+                    true
+                    { barStyle with Fill = Some "green"; Opacity = Some opacity }
+            | false ->
+                createBar
+                    (hValue * -1m)
+                    0m
+                    maxValue
+                    (parameters.MinimumY + ((parameters.MaximumY - parameters.MinimumY) / 2.))
+                    parameters.MaximumY
+                    (parameters.MinimumX + (sectionWidth * float i) + sectionPadding)
+                    barWidth
+                    false
+                    { barStyle with Fill = Some "red"; Opacity = Some opacity })
+        
+    
+    let createMcadLine (parameters: Parameters) (minValue: decimal) (maxValue: decimal) (data: MovingAverageConvergenceDivergence.MacdItem list) =
         let sectionWidth =
             (parameters.MaximumX - parameters.MinimumX) / float parameters.Data.BaseData.Length
         
-        let maxItem =
-            data
-            |> List.maxBy (fun d ->
-                if d.MacdValue > d.SignalEma then
-                    d.MacdValue
-                else
-                    d.SignalEma)
-
-        let minItem =
-            data
-            |> List.minBy (fun d ->
-                if d.MacdValue < d.SignalEma then
-                    d.MacdValue
-                else
-                    d.SignalEma)
-
-        let (maxValue, minValue) =
-            if maxItem.MacdValue > (-1m * minItem.MacdValue) then
-                let v = maxItem.MacdValue + ((maxItem.MacdValue / 100m) * 10m)
-                
-                v, v * -1m
-            else
-                let v = (minItem.MacdValue * -1m) + (((minItem.MacdValue * -1m) / 100m) * 10m)
-                
-                v, v * -1m
-
         let (macdPoints, signalPoints) =
             data
             |> List.mapi (fun i item ->
@@ -91,6 +125,32 @@ module MacdChart =
             |> MovingAverageConvergenceDivergence.generate (MovingAverageConvergenceDivergence.Parameters.Default())
             |> List.take parameters.Data.BaseData.Length
             |> List.rev
+            
+        let maxItem =
+            data
+            |> List.maxBy (fun d ->
+                if d.MacdValue > d.SignalEma then
+                    d.MacdValue
+                else
+                    d.SignalEma)
+
+        let minItem =
+            data
+            |> List.minBy (fun d ->
+                if d.MacdValue < d.SignalEma then
+                    d.MacdValue
+                else
+                    d.SignalEma)
+
+        let (maxValue, minValue) =
+            if maxItem.MacdValue > (-1m * minItem.MacdValue) then
+                let v = maxItem.MacdValue + ((maxItem.MacdValue / 100m) * 10m)
+                
+                v, v * -1m
+            else
+                let v = (minItem.MacdValue * -1m) + (((minItem.MacdValue * -1m) / 100m) * 10m)
+                
+                v, v * -1m
 
 
         [ Line
@@ -112,6 +172,8 @@ module MacdChart =
                 Y2 = parameters.MaximumY
                 Style = parameters.AxisStyle }
 
+          yield! createHistograms parameters minValue maxValue data
+          
           Line
               { X1 = parameters.MinimumX
                 X2 = parameters.MaximumX
@@ -122,4 +184,4 @@ module MacdChart =
                       StrokeDashArray = Some [ 2; 2 ]
                       StrokeWidth = Some 0.5
                       GenericValues = [ "stroke-dashoffset", "1" ] |> Map.ofList } }
-          yield! createMcadLine parameters data ]
+          yield! createMcadLine parameters minValue maxValue data ]
