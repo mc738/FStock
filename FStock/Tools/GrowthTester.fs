bf80bead24ed8c20494e288cdd983e9e9b418348
Max Clifford <me@mclifford.dev>
2024-01-31 18:55:47 +0000

    Added GrowthTester.fs

---
+++ FStock/Tools/GrowthTester.fs
@@ -0,0 +1,11 @@
+﻿namespace FStock.Tools
+
+/// <summary>
+/// The growth tester is used to growth of a position over time.
+/// It is more simplistic than backtesting, instead merely  
+/// </summary>
+[<RequireQualifiedAccess>]
+module GrowthTester =
+    
+    ()
+
