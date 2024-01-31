b0072f8cb980a1b26da6195ad317d2d1bc14cf44
Max Clifford <me@mclifford.dev>
2024-01-31 19:41:34 +0000

    Work on GrowthTester.fs

--- FStock/Analysis/V1/Tools/GrowthTester.fs
+++ FStock/Analysis/V1/Tools/GrowthTester.fs
@@ -25,25 +25,71 @@ module GrowthTester =
         | Or of Condition * Condition
         | Any of Condition list
         | All of Condition list
-        
-        member c.Test(startingPrice: decimal, currentPrice: decimal) =
+
+        member c.Test(startingPrice: decimal, currentPrice: decimal, periodCount: int) =
             match c with
             | Condition.TakeProfit percent ->
-                match 
-                
-                failwith "todo"
-            | Condition.LossStop percent -> failwith "todo"
-            | Condition.FixedPeriod periodSize -> failwith "todo"
-            | Condition.Bespoke(name, fn) -> failwith "todo"
-            | Condition.And(condition, condition1) -> failwith "todo"
-            | Condition.Or(condition, condition1) -> failwith "todo"
-            | Condition.Any conditions -> failwith "todo"
-            | Condition.All conditions -> failwith "todo"
-            
+                match ((startingPrice / 100m) * percent) + startingPrice < currentPrice with
+                | true -> ConditionTestResult.True("take-profit", $"take profit ({percent}%%) hit")
+                | false -> ConditionTestResult.False
+            | Condition.LossStop percent ->
+                match startingPrice - ((startingPrice / 100m) * percent) > startingPrice with
+                | true -> ConditionTestResult.True("loss-stop", $"loss stop ({percent}%%) hit")
+                | false -> ConditionTestResult.False
+            | Condition.FixedPeriod periodSize ->
+                match periodCount >= periodSize with
+                | true -> ConditionTestResult.True("fixed-period", $"fixed period size ({periodSize}) hit")
+                | false -> ConditionTestResult.False
+            | Condition.Bespoke(name, fn) ->
+                match fn startingPrice currentPrice with
+                | true -> ConditionTestResult.True(name, $"bespoke condition {name} hit")
+                | false -> ConditionTestResult.False
+            | Condition.And(condition, condition1) ->
+                match condition.Test(startingPrice, currentPrice, periodCount) with
+                | ConditionTestResult.True(name, message) as ctr ->
+                    match condition1.Test(startingPrice, currentPrice, periodCount) with
+                    | ConditionTestResult.True(name1, message1) ->
+                        ConditionTestResult.True($"{name}_and_{name1}", [ message; message1 ] |> String.concat " and ")
+                    | ConditionTestResult.False -> ConditionTestResult.False
+                | ConditionTestResult.False -> ConditionTestResult.False
+            | Condition.Or(condition, condition1) ->
+                match condition.Test(startingPrice, currentPrice, periodCount) with
+                | ConditionTestResult.True _ as ctr -> ctr
+                | ConditionTestResult.False -> condition1.Test(startingPrice, currentPrice, periodCount)
+            | Condition.Any conditions ->
+                conditions
+                |> List.fold
+                    (fun r c ->
+                        match r with
+                        | ConditionTestResult.True _ -> r
+                        | ConditionTestResult.False -> c.Test(startingPrice, currentPrice, periodCount))
+                    ConditionTestResult.False
+            | Condition.All conditions ->
+                conditions
+                |> List.fold
+                    (fun (r: Result<(string * string) list, unit>) c ->
+                        match r with
+                        | Ok acc ->
+                            match c.Test(startingPrice, currentPrice, periodCount) with
+                            | ConditionTestResult.True(name, message) as ctr -> Ok(acc @ [ name, message ])
+                            | ConditionTestResult.False -> Error()
+                        | Error _ -> r)
+                    (Result.Ok [])
+                |> fun r ->
+                    match r with
+                    | Ok rs ->
+                        let namePaths, messageParts = rs |> List.unzip
+
+                        ConditionTestResult.True(
+                            namePaths |> String.concat "_and_",
+                            messageParts |> String.concat " and "
+                        )
+                    | Error _ -> ConditionTestResult.False
+
     and [<RequireQualifiedAccess>] ConditionTestResult =
-        | True of Type: string * Message : string
+        | True of Type: string * Message: string
         | False
-    
+
     and TestResult =
         | Finished
         | NoResult
@@ -56,5 +102,8 @@ module GrowthTester =
           SellPrice: decimal }
 
 
+    let run () =
+        
+        ()
 
     ()
