ec24a661fa5541cf3985cb509c7f7ebcea806078
Max Clifford <me@mclifford.dev>
2024-02-03 14:49:43 +0000

    Work on Domain.fs

--- FStock/Data/Domain.fs
+++ FStock/Data/Domain.fs
@@ -1,2 +1,26 @@
-﻿module FStock.Data.Domain
+﻿namespace FStock.Data
+
+
+module Domain =
+    
+    
+    /// <summary>
+    /// Represents a want to define open, high, low, close and adjusted close values
+    /// </summary>
+    type [<RequireQualifiedAccess>] OHLCValue =
+        | Open
+        | High
+        | Low
+        | Close
+        | AdjustedClose
+        
+        
+    [<AutoOpen>]    
+    module Extensions =
+        
+        
+        type FStock.Data.Persistence.Records.Stock with
+        
+            member s.GetOHLCValue(ohlc: OHLCValue) =
+                match ohlc
 
