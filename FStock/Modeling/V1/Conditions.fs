e0967c9e90b4053bd20aa4d9a00478baf37572bc
Max Clifford <me@mclifford.dev>
2024-02-03 15:01:48 +0000

    Fix ups to Conditions.fs

--- FStock/Modeling/V1/Conditions.fs
+++ FStock/Modeling/V1/Conditions.fs
@@ -1,7 +1,11 @@
 ﻿namespace FStock.Modeling.V1
 
+open FStock.Data.Domain
+
 [<RequireQualifiedAccess>]
 module Conditions =
+        
+        open FStock.Data.Domain
         
         /// <summary>
         /// A behaviour to check the current position's low value vs a fixed lose.
@@ -9,7 +13,7 @@ module Conditions =
         /// </summary>
         /// <param name="stopLoss"></param>
         let ``fixed stop-loss based on low value`` stopLoss =
-            PositionCondition.FixedLoss(stopLoss, ConditionValueMapper.Value ValueMode.Low)
+            PositionCondition.FixedLoss(stopLoss, ConditionValueMapper.Value OHLCValue.Low)
 
         /// <summary>
         /// A behaviour to check the current position's low value vs a percentage lose.
@@ -17,10 +21,10 @@ module Conditions =
         /// </summary>
         /// <param name="stopLossPercent"></param>
         let ``percentage stop-loss based on low value`` stopLossPercent =
-            PositionCondition.PercentageLoss(stopLossPercent, ConditionValueMapper.Value ValueMode.Low)
+            PositionCondition.PercentageLoss(stopLossPercent, ConditionValueMapper.Value OHLCValue.Low)
 
         let ``fixed take profit based on high value`` takeProfit =
-            PositionCondition.FixedValue(takeProfit, ConditionValueMapper.Value ValueMode.High)
+            PositionCondition.FixedValue(takeProfit, ConditionValueMapper.Value OHLCValue.High)
 
         let ``percentage take profit based on high value`` takeProfitPercent =
-            PositionCondition.PercentageGrowth(takeProfitPercent, ConditionValueMapper.Value ValueMode.High)
+            PositionCondition.PercentageGrowth(takeProfitPercent, ConditionValueMapper.Value OHLCValue.High)
