d838571ed1f84b31d78ddc56dbd6f584eb755fee
Max Clifford <me@mclifford.dev>
2024-02-10 14:49:22 +0000

    Work on StockGrowthTester.fs

---
+++ FStock/Analysis/V1/Tools/StockGrowthTester.fs
@@ -0,0 +1,50 @@
+﻿namespace FStock.Analysis.V1.Tools
+
+open System
+open FStock.Data.Domain
+
+[<RequireQualifiedAccess>]
+module StockGrowthTester =
+    
+    type Parameters =
+        {
+            BuyOHLCValue: OHLCValue
+            SellOHLCValue: OHLCValue
+            TakeProfit: decimal option
+            StopLoss: decimal option
+            MaxPeriods: int
+        }
+        
+        
+    //let getOHLC
+    
+    let tableNamePrefix = "stock_result"
+    
+    let createTableName (parameters: Parameters) (date:DateTime) =
+        let ohlcPart (ohlcValue: OHLCValue) =
+            match ohlcValue with
+            | OHLCValue.Open -> "o"
+            | OHLCValue.High -> "h"
+            | OHLCValue.Low -> "l"
+            | OHLCValue.Close -> "c"
+            | OHLCValue.AdjustedClose -> "ac"
+            | OHLCValue.Volume -> "v"
+        
+        $"{tableNamePrefix}_{date:ddMMyy}_"
+        
+    
+    
+    let createParameters =
+        
+        ()
+    
+    let run () =
+        
+        
+        GrowthTester.run
+        
+        ()
+    
+    
+    ()
+
