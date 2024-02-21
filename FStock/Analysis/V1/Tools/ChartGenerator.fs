dbe0340cd0b2955ae708686de8d418ac28d13bcd
Max Clifford <me@mclifford.dev>
2024-02-21 23:45:21 +0000

    Work on ChartGenerator.fs

--- FStock/Analysis/V1/Tools/ChartGenerator.fs
+++ FStock/Analysis/V1/Tools/ChartGenerator.fs
@@ -2,13 +2,21 @@
 
 module ChartGenerator =
     
+    [<RequireQualifiedAccess>]
     type ChartType =
         | Price
         | Volume
         | Macd
         | Rsi
-        
+    
+    and PriceChartSettings =
+        {
+            Height: float
+            
+        }
      
+    
+    
     
     ()
 
