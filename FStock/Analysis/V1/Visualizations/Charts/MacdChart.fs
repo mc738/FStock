e20a2eab2c2ff5b56e567f569c390565400a0e31
Max Clifford <me@mclifford.dev>
2024-02-21 20:36:16 +0000

    Updated MacdChart.fs

--- FStock/Analysis/V1/Visualizations/Charts/MacdChart.fs
+++ FStock/Analysis/V1/Visualizations/Charts/MacdChart.fs
@@ -8,7 +8,7 @@ module MacdChart =
 
     open FSVG
 
-    type Parameters =
+    type ChartSettings =
         { MinimumX: float
           MaximumX: float
           MinimumY: float
@@ -17,16 +17,25 @@ module MacdChart =
           RightYAxis: bool
           XAxisStartOverride: float option
           XAxisEndOverride: float option
-          AxisStyle: Style
+          AxisStyle: Style }
+
+    type Parameters =
+        { ChartSettings: ChartSettings
           Data: StockData }
 
-    let createHistograms (parameters: Parameters) (minValue: decimal) (maxValue: decimal) (data: MovingAverageConvergenceDivergence.MacdItem list) =
-        
+    let createHistograms
+        (settings: ChartSettings)
+        (minValue: decimal)
+        (maxValue: decimal)
+        (itemCount: int)
+        (data: MovingAverageConvergenceDivergence.MacdItem list)
+        =
+
         let sectionPadding = 0.5
 
         let sectionWidth =
-            (parameters.MaximumX - parameters.MinimumX)
-            / float parameters.Data.BaseData.Length
+            (settings.MaximumX - settings.MinimumX)
+            / float itemCount
 
         let barWidth = sectionWidth - (sectionPadding * 2.)
 
@@ -34,17 +43,17 @@ module MacdChart =
             { Style.Default() with
                 Opacity = Some 1.
                 Fill = Some "blue" }
-        
+
         data
         |> List.mapi (fun i d ->
-            
+
             let prevHValue =
                 match i > 0 with
-                | true -> data.Item (i - 1) |> fun pv -> pv.MacdValue - pv.SignalEma
+                | true -> data.Item(i - 1) |> fun pv -> pv.MacdValue - pv.SignalEma
                 | false -> 0m
-                
+
             let hValue = d.MacdValue - d.SignalEma
-            
+
             let opacity =
                 match hValue > 0m, prevHValue > 0m, hValue >= prevHValue with
                 | true, true, true -> 1.
@@ -53,50 +62,57 @@ module MacdChart =
                 | false, false, true -> 0.5
                 | true, false, _ -> 1.
                 | false, true, _ -> 1.
-                
+
             match hValue > 0m with
             | true ->
                 createBar
                     hValue
                     0m
                     maxValue
-                    parameters.MinimumY
-                    (parameters.MinimumY + ((parameters.MaximumY - parameters.MinimumY) / 2.))
-                    (parameters.MinimumX + (sectionWidth * float i) + sectionPadding)
+                    settings.MinimumY
+                    (settings.MinimumY + ((settings.MaximumY - settings.MinimumY) / 2.))
+                    (settings.MinimumX + (sectionWidth * float i) + sectionPadding)
                     barWidth
                     true
-                    { barStyle with Fill = Some "green"; Opacity = Some opacity }
+                    { barStyle with
+                        Fill = Some "green"
+                        Opacity = Some opacity }
             | false ->
                 createBar
                     (hValue * -1m)
                     0m
                     maxValue
-                    (parameters.MinimumY + ((parameters.MaximumY - parameters.MinimumY) / 2.))
-                    parameters.MaximumY
-                    (parameters.MinimumX + (sectionWidth * float i) + sectionPadding)
+                    (settings.MinimumY + ((settings.MaximumY - settings.MinimumY) / 2.))
+                    settings.MaximumY
+                    (settings.MinimumX + (sectionWidth * float i) + sectionPadding)
                     barWidth
                     false
-                    { barStyle with Fill = Some "red"; Opacity = Some opacity })
-        
-    
-    let createMcadLine (parameters: Parameters) (minValue: decimal) (maxValue: decimal) (data: MovingAverageConvergenceDivergence.MacdItem list) =
+                    { barStyle with
+                        Fill = Some "red"
+                        Opacity = Some opacity })
+
+    let createMcadLine
+        (settings: ChartSettings)
+        (minValue: decimal)
+        (maxValue: decimal)
+        (itemCount: int)
+        (data: MovingAverageConvergenceDivergence.MacdItem list)
+        =
         let sectionWidth =
-            (parameters.MaximumX - parameters.MinimumX) / float parameters.Data.BaseData.Length
-        
+            (settings.MaximumX - settings.MinimumX)
+            / float itemCount
+
         let (macdPoints, signalPoints) =
             data
             |> List.mapi (fun i item ->
 
-                let x =
-                    parameters.MinimumX
-                      + (float i * sectionWidth)
-                      + (sectionWidth / 2.)
-                
+                let x = settings.MinimumX + (float i * sectionWidth) + (sectionWidth / 2.)
+
                 ({ X = x
-                   Y = normalizeYValue item.MacdValue minValue maxValue parameters.MinimumY parameters.MaximumY true }
+                   Y = normalizeYValue item.MacdValue minValue maxValue settings.MinimumY settings.MaximumY true }
                 : SvgPoint),
                 ({ X = x
-                   Y = normalizeYValue item.SignalEma minValue maxValue parameters.MinimumY parameters.MaximumY true }
+                   Y = normalizeYValue item.SignalEma minValue maxValue settings.MinimumY settings.MaximumY true }
                 : SvgPoint))
             |> List.unzip
 
@@ -114,18 +130,20 @@ module MacdChart =
                   { basicStyle with
                       Stroke = Some "orange" } } ]
 
-    let create (parameters: Parameters) =
+    let createFromStocks (settings: ChartSettings) (stocks: StockData) =
+        let itemCount = stocks.BaseData.Length
+        
         let data =
-            parameters.Data.All()
+            stocks.All()
             |> List.map (fun d ->
                 ({ Symbol = ""
                    Date = d.EntryDate
                    Price = d.CloseValue }
                 : BasicInputItem))
             |> MovingAverageConvergenceDivergence.generate (MovingAverageConvergenceDivergence.Parameters.Default())
-            |> List.take parameters.Data.BaseData.Length
+            |> List.take itemCount
             |> List.rev
-            
+
         let maxItem =
             data
             |> List.maxBy (fun d ->
@@ -145,43 +163,42 @@ module MacdChart =
         let (maxValue, minValue) =
             if maxItem.MacdValue > (-1m * minItem.MacdValue) then
                 let v = maxItem.MacdValue + ((maxItem.MacdValue / 100m) * 10m)
-                
+
                 v, v * -1m
             else
                 let v = (minItem.MacdValue * -1m) + (((minItem.MacdValue * -1m) / 100m) * 10m)
-                
+
                 v, v * -1m
 
-
         [ Line
-              { X1 = parameters.MinimumX
-                X2 = parameters.MinimumX
-                Y1 = parameters.MinimumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.MinimumX
+                X2 = settings.MinimumX
+                Y1 = settings.MinimumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
           Line
-              { X1 = parameters.MaximumX
-                X2 = parameters.MaximumX
-                Y1 = parameters.MinimumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.MaximumX
+                X2 = settings.MaximumX
+                Y1 = settings.MinimumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
           Line
-              { X1 = parameters.XAxisStartOverride |> Option.defaultValue parameters.MinimumX
-                X2 = parameters.XAxisEndOverride |> Option.defaultValue parameters.MaximumX
-                Y1 = parameters.MaximumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.XAxisStartOverride |> Option.defaultValue settings.MinimumX
+                X2 = settings.XAxisEndOverride |> Option.defaultValue settings.MaximumX
+                Y1 = settings.MaximumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
 
-          yield! createHistograms parameters minValue maxValue data
-          
+          yield! createHistograms settings minValue maxValue data
+
           Line
-              { X1 = parameters.MinimumX
-                X2 = parameters.MaximumX
-                Y1 = normalizeYValue 50m 0m 100m parameters.MinimumY parameters.MaximumY true
-                Y2 = normalizeYValue 50m 0m 100m parameters.MinimumY parameters.MaximumY true
+              { X1 = settings.MinimumX
+                X2 = settings.MaximumX
+                Y1 = normalizeYValue 50m 0m 100m settings.MinimumY settings.MaximumY true
+                Y2 = normalizeYValue 50m 0m 100m settings.MinimumY settings.MaximumY true
                 Style =
-                  { parameters.AxisStyle with
+                  { settings.AxisStyle with
                       StrokeDashArray = Some [ 2; 2 ]
                       StrokeWidth = Some 0.5
                       GenericValues = [ "stroke-dashoffset", "1" ] |> Map.ofList } }
-          yield! createMcadLine parameters minValue maxValue data ]
+          yield! createMcadLine settings minValue maxValue data ]
