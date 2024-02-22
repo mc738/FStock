9a964e758e0b80b34081967bc03d1678621c22c9
Max Clifford <me@mclifford.dev>
2024-02-22 20:16:48 +0000

    Updated RsiChart.fs

--- FStock/Analysis/V1/Visualizations/Charts/RsiChart.fs
+++ FStock/Analysis/V1/Visualizations/Charts/RsiChart.fs
@@ -24,11 +24,9 @@ module RsiChart =
         { ChartSettings: ChartSettings
           Data: StockData }
 
-    let createRsiLine (settings: ChartSettings) (data: RelativeStrengthIndex.RsiItem list) =
+    let createRsiLine (settings: ChartSettings) (itemCount: int) (data: RelativeStrengthIndex.RsiItem list) =
 
-        let sectionWidth =
-            (settings.MaximumX - settings.MinimumX)
-            / float parameters.Data.BaseData.Length
+        let sectionWidth = (settings.MaximumX - settings.MinimumX) / float itemCount
 
         Path
             { Commands =
@@ -46,45 +44,35 @@ module RsiChart =
                     Stroke = Some "black" } }
 
 
-
-    let create (parameters: Parameters) =
-        let rsiData =
-            parameters.Data.All()
-            |> List.map (fun d ->
-                ({ Symbol = ""
-                   Date = d.EntryDate
-                   Price = d.CloseValue }
-                : BasicInputItem))
-            |> RelativeStrengthIndex.generate (RelativeStrengthIndex.Parameters.Default())
-            |> List.take parameters.Data.BaseData.Length
-            |> List.rev
+    let create (settings: ChartSettings) (data: RelativeStrengthIndex.RsiItem list) =
+        let itemCount = data.Length
 
         [ Line
-              { X1 = parameters.MinimumX
-                X2 = parameters.MinimumX
-                Y1 = parameters.MinimumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.MinimumX
+                X2 = settings.MinimumX
+                Y1 = settings.MinimumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
           Line
-              { X1 = parameters.MaximumX
-                X2 = parameters.MaximumX
-                Y1 = parameters.MinimumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.MaximumX
+                X2 = settings.MaximumX
+                Y1 = settings.MinimumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
           Line
-              { X1 = parameters.XAxisStartOverride |> Option.defaultValue parameters.MinimumX
-                X2 = parameters.XAxisEndOverride |> Option.defaultValue parameters.MaximumX
-                Y1 = parameters.MaximumY
-                Y2 = parameters.MaximumY
-                Style = parameters.AxisStyle }
+              { X1 = settings.XAxisStartOverride |> Option.defaultValue settings.MinimumX
+                X2 = settings.XAxisEndOverride |> Option.defaultValue settings.MaximumX
+                Y1 = settings.MaximumY
+                Y2 = settings.MaximumY
+                Style = settings.AxisStyle }
 
           Line
-              { X1 = parameters.MinimumX
-                X2 = parameters.MaximumX
-                Y1 = normalizeYValue 30m 0m 100m parameters.MinimumY parameters.MaximumY true
-                Y2 = normalizeYValue 30m 0m 100m parameters.MinimumY parameters.MaximumY true
+              { X1 = settings.MinimumX
+                X2 = settings.MaximumX
+                Y1 = normalizeYValue 30m 0m 100m settings.MinimumY settings.MaximumY true
+                Y2 = normalizeYValue 30m 0m 100m settings.MinimumY settings.MaximumY true
                 Style =
-                  { parameters.AxisStyle with
+                  { settings.AxisStyle with
                       Stroke = Some "blue"
                       StrokeWidth = Some 0.5
                       StrokeLineCap = Some StrokeLineCap.Butt
@@ -92,12 +80,12 @@ module RsiChart =
                       GenericValues = [ "stroke-dashoffset", "1" ] |> Map.ofList } }
 
           Line
-              { X1 = parameters.MinimumX
-                X2 = parameters.MaximumX
-                Y1 = normalizeYValue 70m 0m 100m parameters.MinimumY parameters.MaximumY true
-                Y2 = normalizeYValue 70m 0m 100m parameters.MinimumY parameters.MaximumY true
+              { X1 = settings.MinimumX
+                X2 = settings.MaximumX
+                Y1 = normalizeYValue 70m 0m 100m settings.MinimumY settings.MaximumY true
+                Y2 = normalizeYValue 70m 0m 100m settings.MinimumY settings.MaximumY true
                 Style =
-                  { parameters.AxisStyle with
+                  { settings.AxisStyle with
                       Stroke = Some "blue"
                       StrokeWidth = Some 0.5
                       StrokeLineCap = Some StrokeLineCap.Butt
@@ -105,10 +93,10 @@ module RsiChart =
                       GenericValues = [ "stroke-dashoffset", "1" ] |> Map.ofList } }
 
           Rect
-              { Height = normalizeYValue 40m 0m 100m 0 (parameters.MaximumY - parameters.MinimumY) false
-                Width = parameters.MaximumX - parameters.MinimumX
-                X = parameters.MinimumX
-                Y = normalizeYValue 30m 0m 100m parameters.MinimumY parameters.MaximumY false
+              { Height = normalizeYValue 40m 0m 100m 0 (settings.MaximumY - settings.MinimumY) false
+                Width = settings.MaximumX - settings.MinimumX
+                X = settings.MinimumX
+                Y = normalizeYValue 30m 0m 100m settings.MinimumY settings.MaximumY false
                 RX = 0.
                 RY = 0.
                 Style =
@@ -120,4 +108,20 @@ module RsiChart =
                     Opacity = Some 0.2
                     GenericValues = Map.empty } }
 
-          createRsiLine parameters rsiData ]
+          createRsiLine settings itemCount data ]
+
+    let createFromStockData (parameters: StockDataParameters) =
+        let settings = parameters.ChartSettings
+
+        let rsiData =
+            parameters.Data.All()
+            |> List.map (fun d ->
+                ({ Symbol = ""
+                   Date = d.EntryDate
+                   Price = d.CloseValue }
+                : BasicInputItem))
+            |> RelativeStrengthIndex.generate (RelativeStrengthIndex.Parameters.Default())
+            |> List.take parameters.Data.BaseData.Length
+            |> List.rev
+
+        create settings rsiData
