9800e77c25055fe86fe2b6de4d6f28b92297a6e7
Max Clifford <me@mclifford.dev>
2024-02-24 13:37:44 +0000

    Added Instrument

--- FStock/Analysis/V1/Core/Common.fs
+++ FStock/Analysis/V1/Core/Common.fs
@@ -7,6 +7,26 @@ module Common =
 
     open System
 
-    type BasicInputItem = { Symbol: string; Date: DateTime; Price: decimal }
+    type BasicInputItem =
+        { Symbol: string
+          Date: DateTime
+          Price: decimal }
+
+    type Instrument =
+        { Name: string option
+          Symbol: string
+          Type: InstrumentType
+          Entries: InstrumentPositionEntry list }
+
+    and InstrumentPositionEntry =
+        { Date: DateTime
+          Open: decimal
+          High: decimal
+          Low: decimal
+          Close: decimal
+          AdjustedClose: decimal
+          Volume: decimal }
+
+    and InstrumentType = | Stock
 
     let prepend<'T> (item: 'T) (values: 'T list) = item :: values
