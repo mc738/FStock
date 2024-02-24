174883a83bd479b27e78925ee4b68ce96784945b
Max Clifford <me@mclifford.dev>
2024-02-24 14:04:47 +0000

    Work on Common.fs

--- FStock/Analysis/V1/Visualizations/Charts/Common.fs
+++ FStock/Analysis/V1/Visualizations/Charts/Common.fs
@@ -1,6 +1,7 @@
 ﻿namespace FStock.Analysis.V1.Visualizations.Charts
 
 open System
+open FStock.Analysis.V1.Core
 
 [<AutoOpen>]
 module Common =
@@ -11,9 +12,34 @@ module Common =
     type StockData =
         { BaseData: FStock.Data.Persistence.Records.Stock list
           AuxData: FStock.Data.Persistence.Records.Stock list }
-        
-        member sd.All() = (sd.BaseData @ sd.AuxData) |> List.sortBy (fun d -> d.EntryDate) 
-    
+
+        member sd.All() =
+            (sd.BaseData @ sd.AuxData) |> List.sortBy (fun d -> d.EntryDate)
+
+        member sd.BaseInstrumentEntries() =
+            sd.BaseData
+            |> List.map (fun sd ->
+                ({ Date = sd.EntryDate
+                   Open = sd.OpenValue
+                   High = sd.HighValue
+                   Low = sd.LowValue
+                   Close = sd.CloseValue
+                   AdjustedClose = sd.AdjustedCloseValue
+                   Volume = sd.AdjustedCloseValue }
+                : InstrumentPositionEntry))
+            
+        member sd.AuxInstrumentEntries() =
+            sd.AuxData
+            |> List.map (fun sd ->
+                ({ Date = sd.EntryDate
+                   Open = sd.OpenValue
+                   High = sd.HighValue
+                   Low = sd.LowValue
+                   Close = sd.CloseValue
+                   AdjustedClose = sd.AdjustedCloseValue
+                   Volume = sd.AdjustedCloseValue }
+                : InstrumentPositionEntry))
+
     let normalizedValueToPosition (minPos: float) (maxPos: float) (normalizedValue: float) =
         minPos + ((normalizedValue / 100.) * (maxPos - minPos))
 
@@ -90,24 +116,23 @@ module Common =
           StrokeDashArray = None
           Opacity = Some 1.
           GenericValues = Map.empty }
-    
+
     let roundToMultiple (multiple: decimal) (value: decimal) (roundUp: bool) =
         let (v, mpr) =
             match roundUp with
             | true -> System.Math.Ceiling value, MidpointRounding.AwayFromZero
-            | false -> value, MidpointRounding.ToZero 
-        
+            | false -> value, MidpointRounding.ToZero
+
         System.Math.Round(v / multiple, mpr) * multiple
-        
+
     let createMinMaxValues (minValue: decimal) (maxValue: decimal) =
         let diff = maxValue - minValue
-        
+
         let multiple =
             match diff with
             | _ when diff > 1000m -> 100m
             | _ when diff > 100m -> 10m
             | _ when diff > 10m -> 2m
             | _ -> 1m
-            
+
         roundToMultiple multiple minValue false, roundToMultiple multiple maxValue true
-   
