d7323e0dc82458a6c87713a544a8cc80739c763a
Max Clifford <me@mclifford.dev>
2024-02-25 12:26:02 +0000

    Work on ChartGenerator.fs

--- FStock/Analysis/V1/Tools/ChartGenerator.fs
+++ FStock/Analysis/V1/Tools/ChartGenerator.fs
@@ -1,27 +1,30 @@
 ﻿namespace FStock.Analysis.V1.Tools
 
+open Microsoft.FSharp.Core
+
 module ChartGenerator =
-    
+
     [<RequireQualifiedAccess>]
     type ChartType =
         | Price
-        | Volume
+        | Volume 
         | Macd
         | Rsi
-    
-    and PriceChartSettings =
-        {
-            Height: float
-            
-        }
-     
-    
-    and GeneralSettings =
-        {
-            Width: float
-            
-        }
-    
-    
-    ()
 
+    and PriceChartSettings = { Height: float }
+
+    and VolumeChartSettings = { Height: float }
+    
+    and MacdChartSettings = { Height: float }
+    
+    and RsiChartSettings = { Height: float }
+
+    and GeneralSettings =
+        { Width: float
+          LeftPadding: float
+          RightPadding: float
+          TopPadding: float
+          BottomPadding: float }
+
+
+    ()
