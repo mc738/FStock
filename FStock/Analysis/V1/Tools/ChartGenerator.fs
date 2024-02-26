2cbca5d450a14514412ac344393b95b7855b2bce
Max Clifford <me@mclifford.dev>
2024-02-26 19:21:09 +0000

    Work on ChartGenerator.fs

--- FStock/Analysis/V1/Tools/ChartGenerator.fs
+++ FStock/Analysis/V1/Tools/ChartGenerator.fs
@@ -1,7 +1,12 @@
 ﻿namespace FStock.Analysis.V1.Tools
 
+open System
 open FSVG
+open FStock.Analysis.V1.TechnicalIndicators
 open FStock.Analysis.V1.Visualizations.Charts
+
+open Freql.Sqlite
+open Microsoft.Data.Sqlite
 open Microsoft.FSharp.Core
 
 module ChartGenerator =
@@ -13,7 +18,10 @@ module ChartGenerator =
         | Macd of MacdChartSettings
         | Rsi of RsiChartSettings
 
-    and PriceChartSettings = { Height: float }
+    and PriceChartSettings =
+        { Height: float
+          ShortMovingAverageTableName: string
+          LongMovingAverageTableName: string }
 
     and VolumeChartSettings = { Height: float }
 
@@ -45,13 +53,42 @@ module ChartGenerator =
                 CurrentY = currentY
                 Elements = gs.Elements @ elements }
 
+    let fetchSimpleMovingAverageData
+        (storeCtx: SqliteContext)
+        (tableName: string)
+        (symbol: string)
+        (endDate: DateTime)
+        (entryCount: int)
+        =
+        let sql, parameters =
+            $"""SELECT * FROM {tableName} WHERE symbol = @0 AND DATE(entry_date) <= DATE(@1) ORDER BY DATE(entry_date) DESC LIMIT @2""",
+            [ box symbol; box endDate; box entryCount ]
+
+        storeCtx.SelectAnon<SimpleMovingAverage.SmaItem>(sql, parameters)
+        |> List.sortBy (fun sma -> sma.EntryDate)
+
+    let fetchRsiData
+        (storeCtx: SqliteContext)
+        (tableName: string)
+        (symbol: string)
+        (endDate: DateTime)
+        (entryCount: int)
+        =
+
+        let sql, parameters =
+            $"""SELECT * FROM {tableName} WHERE symbol = @0 AND DATE(entry_date) <= DATE(@1) ORDER BY DATE(entry_date) DESC LIMIT @2""",
+            [ box symbol; box endDate; box entryCount ]
+
+        storeCtx.SelectAnon<RelativeStrengthIndex.RsiItem>(sql, parameters)
+        |> List.sortBy (fun rsi -> rsi.EntryDate)
+
+
+    
 
     let generate (settings: GeneratorSettings) =
         let initState = GeneratorState.Create(yStart = settings.Settings.TopPadding)
 
-        settings.Parts
-        |> List.fold (fun state ct ->
-            state) initState
+        settings.Parts |> List.fold (fun state ct -> state) initState
 
 
 
